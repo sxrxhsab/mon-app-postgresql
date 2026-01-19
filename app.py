f7ab02e1acfb119da26f050de783745a1afadebd
kahina BENMOUSSA <148722037+kahina1978@users.noreply.github.com>
2026-01-19 20:40:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -254,6 +254,5 @@ def conflits_admin():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render fournit le PORT via variable d'environnement
-    app.run(host="0.0.0.0", port=port, debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
